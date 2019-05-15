2ecd4499ae28ecb59bfdca1521ec6091ce5ac8ca
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-15 15:14:27 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  def initialize(name, breed)
+    @dog_name = name
+    @dog_breed = breed
+  end
+
+  def name=(doggo_name)
+    @dog_name = doggo_name
+  end
+
+  def name
+    @dog_name
+  end
+
+  
+    
