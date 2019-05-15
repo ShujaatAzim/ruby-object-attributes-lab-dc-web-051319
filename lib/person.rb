9a76bcc3662ad5cac1ed1680f92c971694a76e17
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-15 15:19:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,18 @@ class Person
   def name=(persons_name)
     @name = persons_name
   end
-  
+
+  def name
+    @name
+  end
 
   def job=(persons_job)
     @job = persons_job
   end
+
+  def job
+    @job
+  end
+
 
 end
